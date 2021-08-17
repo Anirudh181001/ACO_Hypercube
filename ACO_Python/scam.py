9e80ed5f0607d0ac86d5fa0e608b8be2ca5e6682
Dhanushkmr <Dhanush5247@gmail.com>
2021-08-17 15:10:36 +0800

    multiprocessed montecarlo

--- ACO_Python/scam.py
+++ ACO_Python/scam.py
@@ -1,6 +1,29 @@
-# print("self.number: 0\nself.curr_color: red\nself.history_vertices: [(0, 0, 0, 0), (0, 1, 0, 0), (0, 1, 0, 1), (1, 1, 0, 1), (1, 1, 1, 1)]\nself.is_violated: False\nself.last_visited: (1, 1, 1, 1)\nself.history_colours: ['red', 'blue', 'red']\nself.last_vertex_before_color_change: (1, 1, 1, 1)\nNone\nself.number: 0\nself.curr_color: blue\nself.history_vertices: [(0, 0, 0, 0), (0, 0, 1, 0), (0, 0, 1, 1), (1, 0, 1, 1), (1, 1, 1, 1)]\nself.is_violated: False\nself.last_visited: (1, 1, 1, 1)\nself.history_colours: ['red', 'blue', 'red', 'blue']\nself.last_vertex_before_color_change: (0, 0, 1, 1)\nNone\nself.number: 4\nself.curr_color: blue\nself.history_vertices: [(0, 0, 0, 0), (0, 1, 0, 0), (0, 1, 0, 1), (1, 1, 0, 1), (1, 1, 1, 1)]\nself.is_violated: False\nself.last_visited: (1, 1, 1, 1)\nself.history_colours: ['blue', 'red', 'blue']\nself.last_vertex_before_color_change: (1, 1, 1, 1)\nNone\nself.number: 0\nself.curr_color: red\nself.history_vertices: [(0, 0, 0, 0), (0, 0, 1, 0), (1, 0, 1, 0), (1, 1, 1, 0), (1, 1, 1, 1)]\nself.is_violated: False\nself.last_visited: (1, 1, 1, 1)\nself.history_colours: ['blue', 'red', 'blue', 'red', 'blue', 'red']\nself.last_vertex_before_color_change: (0, 0, 1, 0)\nNone\nself.number: 1\nself.curr_color: blue\nself.history_vertices: [(0, 0, 0, 0), (0, 1, 0, 0), (1, 1, 0, 0), (1, 1, 0, 1), (1, 1, 1, 1)]\nself.is_violated: False\nself.last_visited: (1, 1, 1, 1)\nself.history_colours: ['blue', 'red', 'blue']\nself.last_vertex_before_color_change: (1, 1, 1, 1)\nNone\nself.number: 3\nself.curr_color: red\nself.history_vertices: [(0, 0, 0, 0), (1, 0, 0, 0), (1, 1, 0, 0), (1, 1, 0, 1), (1, 1, 1, 1)]\nself.is_violated: False\nself.last_visited: (1, 1, 1, 1)\nself.history_colours: ['red', 'blue', 'red']\nself.last_vertex_before_color_change: (1, 1, 1, 1)\nNone\nTime taken to run is: 5.052884101867676 seconds\n")
-import statistics
-import numpy as np
-scam = [1,2,3,4,5,np.NaN]
-print(np.nanmean(scam))
-# print(statistics.mean(scam))
+from ACO_Hypercube_PreDetermined_Random import run_ants_on_hypercube_random_colors
+from ACO_Hypercube_PreDetermined_Random_Pheromone import run_ants_on_hypercube_random_colors_optimized
+
+def hypercube_monte_carlo_random(num_points = 100, N = 7, num_ants = 10, fn = run_ants_on_hypercube_random_colors):
+    iter_list = []
+    path_len_list = []
+    ant_winner_list = []
+    n_matchings_list = []
+    for iter in range(num_points):
+        (ant_winner, path_len), iter,  n_matchings = fn(N, num_ants, False, True)
+        iter_list.append(iter)
+        path_len_list.append(path_len)
+        ant_winner_list.append(ant_winner)
+        n_matchings_list.append(n_matchings)
+    return (iter_list, path_len_list, ant_winner_list, n_matchings_list)
+
+
+def hypercube_monte_carlo_optimised(num_points = 100, N = 7, num_ants = 10, fn = run_ants_on_hypercube_random_colors_optimized):
+    iter_list = []
+    path_len_list = []
+    ant_winner_list = []
+    n_matchings_list = []
+    for iter in range(num_points):
+        (ant_winner, path_len), iter,  n_matchings = fn(N, num_ants, False, True)
+        iter_list.append(iter)
+        path_len_list.append(path_len)
+        ant_winner_list.append(ant_winner)
+        n_matchings_list.append(n_matchings)
+    return (iter_list, path_len_list, ant_winner_list, n_matchings_list)
